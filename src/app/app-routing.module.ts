f8156911a89382606a8529bdc927e0d45cc12c15
jmedinac1987 <jmedinac1987@gmail.com>
2018-09-27 16:22:03 -0500

    Ajuste de rutas principales e inclusion  de favicon personalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,13 @@ import { homeRoutes } from './components/navbarhome/home.routing';
 import { profileRoutes } from './components/navbarprofile/profile.routing';
 import { BeforeLoginService } from './services/before-login.service';
 import { AfterLoginService } from './services/after-login.service';
-import { TokenService } from "./services/token.service";
+/*import { TokenService } from "./services/token.service";
 
 const token: TokenService = new TokenService();
-const isAuth = token.isValidToken() ? { path:'', redirectTo: '/profile', pathMatch:'full' }: { path:'', redirectTo: '/home', pathMatch:'full' };
+const isAuth = token.isValidToken() ? { path:'', redirectTo: '/profile', pathMatch:'full' }: { path:'', redirectTo: '/home', pathMatch:'full' };*/
 
 const routes: Routes = [
-  isAuth,
+  { path:'', redirectTo: '/home', pathMatch:'full' },
   { path:'home', component: NavbarhomeComponent, children: homeRoutes, canActivate: [BeforeLoginService] },  
   { path:'profile', component: NavbarprofileComponent, children: profileRoutes, canActivate: [AfterLoginService] },  
   { path:'**', component: NotFoundComponent }
